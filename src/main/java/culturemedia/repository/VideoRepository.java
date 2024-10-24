8f9aee74734132fd21f1985100546f26f583877a
Emanuel Zuluaga <emanuel.zuluagap@udea.edu.co>
2024-10-23 21:41:41 -0500

    Adding video structures

--- src/main/java/culturemedia/repository/VideoRepository.java
+++ src/main/java/culturemedia/repository/VideoRepository.java
@@ -6,6 +6,6 @@ import culturemedia.model.Video;
 public interface VideoRepository {
     List<Video> findAll();
     Video add(Video video);
-    List<Video> findByTitle(String title);
-    List<Video> findDuration(Double lengthFrom, Double lengthGoesUp);
+    List<Video> find(String title);
+    List<Video> find(Double lengthFrom, Double lengthGoesUp);
 }
