2f49991705df2941814a54e0401fd38b22e34df9
Emanuel Zuluaga <emanuel.zuluagap@udea.edu.co>
2024-11-02 18:29:51 -0500

    Adding video structures

---
+++ src/test/java/culturemedia/service/CultureMediaServiceTest.java
@@ -0,0 +1,49 @@
+package culturemedia.service;
+
+import culturemedia.exception.VideoNotFoundException;
+import culturemedia.model.Video;
+import culturemedia.repository.VideoRepository;
+import culturemedia.repository.impl.VideoRepositoryImpl;
+import culturemedia.repository.impl.ViewsRepositoryImpl;
+import culturemedia.service.impl.CultureMediaServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CultureMediaServiceTest {
+    private CultureMediaService cultureMediaService;
+
+    @BeforeEach
+    void init(){
+        cultureMediaService = new CultureMediaServiceImpl(new ViewsRepositoryImpl(), new VideoRepositoryImpl());
+    }
+
+    void save_video(){
+        List<Video> videos = List.of(new Video("01", "Título 1", "----", 4.5),
+                new Video("02", "Título 2", "----", 5.5),
+                new Video("03", "Título 3", "----", 4.4),
+                new Video("04", "Título 4", "----", 3.5),
+                new Video("05", "Clic 5", "----", 5.7),
+                new Video("06", "Clic 6", "----", 5.1));
+
+
+        for ( Video video : videos ) {
+            cultureMediaService.addVideo( video );
+        }
+    }
+
+    @Test
+    void when_FindAll_all_videos_should_be_returned_successfully() throws VideoNotFoundException {
+        save_video();
+        List<Video> videos = cultureMediaService.findAll();
+        assertEquals(6, videos.size());
+    }
+
+    @Test
+    void when_FindAll_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully() {
+        assert(false);
+    }
+}
