8537d8c37e6dadde2b43fb17be8cceedb5461e00
Emanuel Zuluaga <emanuel.zuluagap@udea.edu.co>
2024-11-02 20:02:56 -0500

    Adding video structures

--- src/main/java/culturemedia/controller/CultureMediaController.java
+++ src/main/java/culturemedia/controller/CultureMediaController.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
+import culturemedia.model.View;
+import culturemedia.service.CultureMediaService;
 import culturemedia.service.impl.CultureMediaServiceImpl;
 
-public class CultureMediaController{
+public class CultureMediaController implements CultureMediaService {
 
     private final CultureMediaServiceImpl cultureMediaService;
 
@@ -20,6 +22,26 @@ public class CultureMediaController{
         List<Video> videos = null;
         videos = cultureMediaService.findAll();
         return videos;
+    }
+
+    @Override
+    public View addView(View view) {
+        return cultureMediaService.addView(view);
+    }
+
+    @Override
+    public List<Video> findAll() throws VideoNotFoundException {
+        List<Video> videos = null;
+        videos = cultureMediaService.findAll();
+        if (videos.isEmpty()){
+            throw new VideoNotFoundException("Video no encontrado");
+        }
+        return videos;
+    }
+
+    @Override
+    public Video addVideo(Video video) {
+        return cultureMediaService.addVideo(video);
     }
 }
 
