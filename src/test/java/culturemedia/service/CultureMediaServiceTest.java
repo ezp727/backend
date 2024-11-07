391d48289e3c02d91ce451fc8594b4f942e59675
Emanuel Zuluaga <emanuel.zuluagap@udea.edu.co>
2024-11-06 22:11:55 -0500

    Adding video structures

--- src/test/java/culturemedia/service/CultureMediaServiceTest.java
+++ src/test/java/culturemedia/service/CultureMediaServiceTest.java
@@ -3,37 +3,44 @@ package culturemedia.service;
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
 import culturemedia.repository.VideoRepository;
-import culturemedia.repository.impl.VideoRepositoryImpl;
-import culturemedia.repository.impl.ViewsRepositoryImpl;
+import culturemedia.repository.ViewsRepository;
 import culturemedia.service.impl.CultureMediaServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class CultureMediaServiceTest {
+
     private CultureMediaService cultureMediaService;
+    private VideoRepository videoRepositoryMock;
 
     @BeforeEach
     void init(){
-        cultureMediaService = new CultureMediaServiceImpl(new ViewsRepositoryImpl(), new VideoRepositoryImpl());
+
+        videoRepositoryMock = Mockito.mock(VideoRepository.class);
+        ViewsRepository viewsRepositoryMock = Mockito.mock(ViewsRepository.class);
+
+        cultureMediaService = new CultureMediaServiceImpl(viewsRepositoryMock, videoRepositoryMock);
     }
 
-    void save_video(){
-        List<Video> videos = List.of(new Video("01", "Título 1", "----", 4.5),
+    void save_video() {
+        // lista
+        List<Video> videos = List.of(
+                new Video("01", "Título 1", "----", 4.5),
                 new Video("02", "Título 2", "----", 5.5),
                 new Video("03", "Título 3", "----", 4.4),
                 new Video("04", "Título 4", "----", 3.5),
                 new Video("05", "Clic 5", "----", 5.7),
-                new Video("06", "Clic 6", "----", 5.1));
+                new Video("06", "Clic 6", "----", 5.1)
+        );
 
-
-        for ( Video video : videos ) {
-            cultureMediaService.addVideo( video );
-        }
+        // findall
+        when(videoRepositoryMock.findAll()).thenReturn(videos);
     }
 
     @Test
@@ -44,8 +51,10 @@ class CultureMediaServiceTest {
     }
 
     @Test
-    void when_FindAll_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully() throws VideoNotFoundException{
-        init();    //preguntar
-        assertThrows(VideoNotFoundException.class,()->{cultureMediaService.findAll();});
+    void when_FindAll_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully() {
+        // findall no encuentra
+        when(videoRepositoryMock.findAll()).thenThrow(new VideoNotFoundException("No videos found"));
+
+        assertThrows(VideoNotFoundException.class, () -> cultureMediaService.findAll());
     }
 }
